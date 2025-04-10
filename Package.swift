ae4e19c96593390bcb760788547551ec75092510
Jon Schneider <jon@jonschneider.me>
2025-04-10 09:48:57 -0400

    Updates Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,23 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(name: "TagListView",
-                      platforms: [.iOS(.v15)],
-                      products: [.library(name: "TagListView",
-                                          targets: ["TagListView"])],
-                      targets: [.target(name: "TagListView",
-                                        path: "TagListView")],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "TagListView",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "TagListView",
+            targets: ["TagListView"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "TagListView",
+            path: "TagListView"
+        )
+    ]
+)
